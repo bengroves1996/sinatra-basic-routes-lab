6a11eaad25ad7dc3ecad62e1b801166c9d233249
bengroves <bengroves15@gmail.com>
2020-11-27 19:47:53 -0500

    commit tree 96c5842832d797b5924e4583066fe300ade2d916

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+    end 
+    
+    get '/name' do
+        "My name is Ben"
+    end 
+
+    get '/hometown' do
+        "My hometown is Hollis"
+    end 
+
+    get '/favorite-song' do
+        "My favorite song is the Hamster Song"
+    end 
+    
+
 end
